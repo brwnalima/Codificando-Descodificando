b1fca507c763616cc829904c45697044147249a1
brwnalima <bruna.lima@edu.unirio.br>
2022-10-05 10:18:22 -0300

    update 3

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -1,19 +1,15 @@
 // Declando variáveis importantes para me ajudar //
 
 var seletor = document.querySelector("select")
-var botaoConf = document.querySelector("btn-escolha")
-
 var addContainer = document.getElementById("divEscondida")
-
-var texto = document.querySelector("#msg-inicio");
+var texto = document.querySelector("#msg-inicio")
 var txtResultado = document.querySelector("#msg-result")
+var radioCode = document.getElementById("#codificar")
+var radioDecode = document.getElementById("#decodificar")
+var btnCodificar = document.getElementById("#btn-codificar")
 
-var btnCodificar = document.querySelector("#btn-codificar")
-var btnDecodificar = document.querySelector("#btn-descodificar")
 
-
-//Evento criado para quando o select mudar para o Cifra de Cesar
-//a div escondida (passo) aparecer.
+//Evento criado para quando o select mudar para o Cifra de Cesar a div escondida (passo) aparecer.
 seletor.addEventListener("change", function(event){
     
     if (event.target.value == "cifraCesar") { //se o value for o value cifraCesar
@@ -28,39 +24,22 @@ seletor.addEventListener("change", function(event){
 
     });
 
+// Dependendo do radio selecionado, o botão btnCodificar muda a frase
+
+var fraseCode = "Codificar texto"
+var fraseDecode = "Decodificar texto"
+
+radioCode.addEventListener("click", function() {
+    btnCodificar.innerText = fraseCode
+  });
+  
+
+radioDecode.addEventListener("click", function() {
+   console.log(btnCodificar.innerText = fraseDecode)
+  });
+
 //Evento criado para que o click am algum botão não recarregue a pág
-botao.addEventListener("click", function(event){
+btnCodificar.addEventListener("check", function(event){
     console.log(texto.value);
     event.preventDefault()
 });
-
-
-btnCodificar.addEventListener("click", function(event) {
-
-    if (seletor.target.value == "base.64"){
-       txtResultado = codeBase(texto)
-      //codificar base 64/
-    }
-
-});
-  
-btnDecodificar.addEventListener("click", function() {
-
-    if(btnDecodificar.checked && seletor.value == "cifraCesar"){
-        txtResultado.value = decifra(parseInt(passo.value), texto.value);
-    //decodificar cifra//
-    }else if (decodificar.checked && seletor.value == "base.64") {
-        txtResultado.value = decodeBase(texto.value)
-    //decodificar base//
-    }
- 
-})
-
-//Funções de codificação e descodificação em Base64
-function codeBase (texto) {
-    return btoa(texto)
-}
-
-function decodeBase (texto) {
-    return atob(texto) 
-}
